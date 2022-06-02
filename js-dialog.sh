cbc0a3e2cb6947c3857d73da9901c8c0d629c98b
Arafat Husayn <hello@arafat.dev>
2022-06-02 17:28:24 +0600

    take node.js binary path from first argument

--- js-dialog.sh
+++ js-dialog.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-_CODE=$(zenity --text-info --editable --height=200 --width=600 --title="JavaScript Code Input (Node.js $(node -v))")
+_NODEJS_BIN="$1"
 
-_STRING=\`$(node -p "{ $_CODE }" 2>&1)\`
+if [[ $# -eq 0 ]]; then
+  _NODEJS_BIN=node
+fi
 
-_OUTPUT=$(node -p "$(echo "$_STRING").replace(/\nundefined$/g, '')")
+_CODE=$(zenity --text-info --editable --height=200 --width=600 --title="JavaScript Code Input (Node.js $($_NODEJS_BIN -v))")
+
+_STRING=\`$($_NODEJS_BIN -p "{ $_CODE }" 2>&1)\`
+
+_OUTPUT=$($_NODEJS_BIN -p "$(echo "$_STRING").replace(/\nundefined$/g, '')")
 
 echo "$_OUTPUT" | zenity --text-info --height=500 --width=600 --title="Output"
